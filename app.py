e501459005c5d6a8e7458d03fcb10d457a1236a1
Aaron Rotman <58377413+aaronrotman@users.noreply.github.com>
2020-08-12 12:49:38 -0700

    Create flask app to handle user input submissions

--- app.py
+++ app.py
@@ -1,23 +1,44 @@
-# import necessary libraries
-from flask import Flask, render_template
+# Dependencies
+from flask import Flask, render_template, request, redirect
 
-# create instance of Flask app
+# Create instance of Flask app
 app = Flask(__name__)
 
+# Route that renders the welcome page and receives user inputs
+@app.route("/", methods=["GET", "POST"])
+def user_inputs():
 
-# create route that renders index.html template
-# List of dictionaries
-# dogs = [{"name": "Fido", "type": "Lab"},
-#         {"name": "Rex", "type": "Collie"},
-#         {"name": "Suzzy", "type": "Terrier"},
-#         {"name": "Tomato", "type": "Retriever"}]
+    if request.method == "POST":
+
+        req = request.form
+
+        summer_temp = req["summer-temp"]
+        winter_temp = req["winter-temp"]
+        city_size = req["city-size"]
+        house_size = req["house-size"]
+        budget = req["budget"]
+        bedrooms = req["bedrooms"]
+        bathrooms = req["bathrooms"]
+        yard = req["yard"]
+    
+
+        print(
+            f"""
+            Form submitted:\n
+            Summer Temp: {summer_temp}\n
+            Winter Temp: {winter_temp}\n
+            City Size: {city_size}\n
+            House Size: {house_size}\n
+            Budget: {budget}\n
+            Bedrooms: {bedrooms}\n
+            Bathrooms: {bathrooms}\n
+            Yard: {yard}\n 
+            """)
 
 
-# create route that renders index.html template
-@app.route("/")
-def index():
+        return redirect(request.url)
 
-    return render_template("display_results.html", dogs=dogs)
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
