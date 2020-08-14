f7419c096509cbb87eacc8cda60ca7d9a2a06213
Cynthia <59157682+cynthiakellogg@users.noreply.github.com>
2020-08-14 11:20:46 -0700

    added the pickle

--- the_magic.py
+++ the_magic.py
@@ -1,3 +1,4 @@
+
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neighbors import NearestNeighbors
@@ -13,63 +14,25 @@ from sklearn.preprocessing import StandardScaler
 import warnings
 warnings.simplefilter('ignore')
 import numpy as np
+import pickle
 
-def run_ml():
-# importing data
-    housing_training_data = pd.read_csv("housing_training_data.csv")
-    house_data_for_results = pd.read_csv("combination_data/housing_complete_data.csv")
-    X = housing_training_data[['price', "beds", 'baths', "square_feet", "lot_size", 'hoa_permonth', "Summer Temp", "Winter Temp", "population"]]
-    data = X.copy()
+housing_training_data = pd.read_csv("housing_training_data.csv")
+house_data_for_results = pd.read_csv("combination_data/housing_complete_data.csv")
+X = housing_training_data[["Summer Temp", "Winter Temp", "pop_cat_hot", "square_feet", 'price', "beds", 'baths', "lot_size_hot"]]
+
+
+def train_and_save_model():
+
     from sklearn.model_selection import train_test_split
-    X_train, X_test= train_test_split(X, random_state=42)
-    from sklearn.preprocessing import StandardScaler
-    X_scaler = StandardScaler().fit(X)
-    X_train_scaled = X_scaler.transform(X)
-    kmeans = KMeans(n_clusters=200)
-    kmeans.fit(X)
-    predicted_clusters = kmeans.predict(X)
-    Xlist = X.to_numpy()
-
-
-    # ### HERE BEGINS THE MAGIC unsupervised cluster 
-    kmeans.fit(Xlist)
-    predicted_clusters = kmeans.predict(Xlist)
-    # Print the cluster centers and cluster labels
-    # centers = kmeans.cluster_centers_
-    # labels = kmeans.labels_
-    cat = kmeans.predict(Xlist[0].reshape(1, -1))
-    house_list = []
-
-    for i in range(4000):
-        if kmeans.predict(Xlist[i].reshape(1, -1)) == cat:
-    #         print (housing_training_data.iloc[i])
-    #         print (housing_training_data.house_id[i])
-            house_list.append(housing_training_data.house_id[i])
-    #         print (Xlist[i])
-    #         print (kmeans.predict(Xlist[i].reshape(1, -1)))
-            
-        
-    results_df = house_data_for_results[house_data_for_results["house_id"].isin(house_list)]
-
-    return results_df
-
-# results_df.to_html(classes="results")
-def make_prediction(input_array):
-    # Change this to load saved model
-    # importing data
-    housing_training_data = pd.read_csv("housing_training_data.csv")
-    house_data_for_results = pd.read_csv("combination_data/housing_complete_data.csv")
-    X = housing_training_data[["Summer Temp", "Winter Temp", "pop_cat_hot", "square_feet", 'price', "beds", 'baths', "lot_size_hot"]]
-    # data = X.copy()
-    from sklearn.model_selection import train_test_split
-    # X_train, X_test= train_test_split(X, random_state=42)
-    from sklearn.preprocessing import StandardScaler
-    X_scaler = StandardScaler().fit(X)
-    # X_train_scaled = X_scaler.transform(X)
     kmeans = KMeans(n_clusters=400)
     kmeans.fit(X)
-    # predicted_clusters = kmeans.predict(X)
-    Xlist = X.to_numpy()
+    
+    with open('kmeans.pickle', 'wb') as f:
+        pickle.dump(kmeans, f)
+
+train_and_save_model()    
+
+def make_prediction(input_array):    
     # Convert string input to number
     if input_array[2] == "Small Town":
         input_array[2] = 0
@@ -83,24 +46,19 @@ def make_prediction(input_array):
         input_array[7] = 1
     else:
         input_array[7] = 0
-
+    
+    Xlist = X.to_numpy()
     # ### HERE BEGINS THE MAGIC unsupervised cluster 
+    with open('kmeans.pickle', 'rb') as f:
+        kmeans = pickle.load(f)
+
     kmeans.fit(Xlist)
-    # predicted_clusters = kmeans.predict(Xlist)
-    # Print the cluster centers and cluster labels
-    # centers = kmeans.cluster_centers_
-    # labels = kmeans.labels_
     cat = kmeans.predict(np.array(input_array).reshape(1, -1))
     house_list = []
 
     for i in range(len(housing_training_data)):
         if kmeans.predict(Xlist[i].reshape(1, -1)) == cat:
-    #         print (housing_training_data.iloc[i])
-    #         print (housing_training_data.house_id[i])
             house_list.append(housing_training_data.house_id[i])
-    #         print (Xlist[i])
-    #         print (kmeans.predict(Xlist[i].reshape(1, -1)))
-            
         
     results_df = house_data_for_results[house_data_for_results["House ID"].isin(house_list)]
 
