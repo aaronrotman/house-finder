4a808eed30c5fb47603c57bccf9c6c4697000634
Cynthia <59157682+cynthiakellogg@users.noreply.github.com>
2020-08-13 21:04:44 -0700

    House ID

--- the_magic.py
+++ the_magic.py
@@ -60,15 +60,15 @@ def make_prediction(input_array):
     housing_training_data = pd.read_csv("housing_training_data.csv")
     house_data_for_results = pd.read_csv("combination_data/housing_complete_data.csv")
     X = housing_training_data[["Summer Temp", "Winter Temp", "pop_cat_hot", "square_feet", 'price', "beds", 'baths', "lot_size_hot"]]
-    data = X.copy()
+    # data = X.copy()
     from sklearn.model_selection import train_test_split
-    X_train, X_test= train_test_split(X, random_state=42)
+    # X_train, X_test= train_test_split(X, random_state=42)
     from sklearn.preprocessing import StandardScaler
     X_scaler = StandardScaler().fit(X)
-    X_train_scaled = X_scaler.transform(X)
+    # X_train_scaled = X_scaler.transform(X)
     kmeans = KMeans(n_clusters=200)
     kmeans.fit(X)
-    predicted_clusters = kmeans.predict(X)
+    # predicted_clusters = kmeans.predict(X)
     Xlist = X.to_numpy()
     # Convert string input to number
     if input_array[2] == "Small Town":
@@ -86,7 +86,7 @@ def make_prediction(input_array):
 
     # ### HERE BEGINS THE MAGIC unsupervised cluster 
     kmeans.fit(Xlist)
-    predicted_clusters = kmeans.predict(Xlist)
+    # predicted_clusters = kmeans.predict(Xlist)
     # Print the cluster centers and cluster labels
     # centers = kmeans.cluster_centers_
     # labels = kmeans.labels_
@@ -102,7 +102,7 @@ def make_prediction(input_array):
     #         print (kmeans.predict(Xlist[i].reshape(1, -1)))
             
         
-    results_df = house_data_for_results[house_data_for_results["house_id"].isin(house_list)]
+    results_df = house_data_for_results[house_data_for_results["House ID"].isin(house_list)]
 
     return results_df
     
